280e35c8caeae84df930642ae3c2855b333a8985
saquib <saquibm860@gamil.com>
2024-07-06 16:03:09 +0530

    Constant page is created for about us page.

---
+++ lms-frontend/src/Constant/CelebrityData.js
@@ -0,0 +1,39 @@
+import apj from '../Assets/Images/QuotesPersonalityImage/apj.png'
+import billGates from '../Assets/Images/QuotesPersonalityImage/billGates.png'
+import einstein from '../Assets/Images/QuotesPersonalityImage/einstein.png'
+import nelsonMandela from '../Assets/Images/QuotesPersonalityImage/nelsonMandela.png'
+import steveJobs from '../Assets/Images/QuotesPersonalityImage/steveJobs.png'
+
+
+export const Celebrities = [
+    {
+      title: "APJ Abdul Kalam Azad",
+      quote: "“You have to dream before your dreams can come true.”",
+      image: apj,
+      slideNumber: 1
+    },
+    {
+      title: "Bill Gates",
+      quote: "“It's fine to celebrate success but it is more important to heed the lessons of failure.”",
+      image: billGates,
+      slideNumber: 2
+    },
+    {
+      title: "Albert Einstein",
+      quote: "“You have to dream before your dreams can come true.”",
+      image: einstein,
+      slideNumber: 3
+    },
+    {
+      title: "Nelson Mandela",
+      quote: "“Do not judge me by my successes, judge me by how many times I fell down and got back up again.”",
+      image: nelsonMandela,
+      slideNumber: 4
+    },
+    {
+      title: "Steve Jobs",
+      quote: "“Innovation distinguishes between a leader and a follower.”",
+      image: steveJobs,
+      slideNumber: 5
+    }
+];
